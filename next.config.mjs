3b96601d29a32473d6e315f3f66dc8581c2853e2
Md Rabiul Alam <148884849+alamrb@users.noreply.github.com>
2024-11-30 19:17:29 +0600

    Update next.config.mjs
    dsfsfa

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/myapp',  // Defines the base path for your app
-  assetPrefix: 'https://myapp.netnurture.uk',  // Defines where your static assets will be served from
+  // basePath: '/myapp',  // Defines the base path for your app
+  // assetPrefix: 'https://myapp.netnurture.uk',  // Defines where your static assets will be served from
 };
 
 export default nextConfig;
